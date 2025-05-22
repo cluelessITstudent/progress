520169281574a39b7ab4076f6d021223d640b6ff
cluelessITstudent <112802134+cluelessITstudent@users.noreply.github.com>
2025-05-21 23:32:02 -0700

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://tORbzrXUnuQBZ1TKcA7PnWH5tSRrqjQ1_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "5 Tips for Better Naps"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
